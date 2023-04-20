0f0f076adca4e6e872daef0dc4ee0b3037e693f6
syafiul-anam <syafiulanammu@gmail.com>
2023-04-20 13:00:54 +0700

    trial

--- TA_Yogo_UNO.ino
+++ TA_Yogo_UNO.ino
@@ -77,10 +77,41 @@ DallasTemperature temperature(&oneWire);
  */
 #include <SPI.h>
 #include <SD.h>
+#define SD_CS_Pin           53      // pin 53 Arduino Mega
+#define LOG_NUMBER_FILE     "number.txt"
 File myFile;
-// #include "SdFat.h"
-// SdFat sd;
-// SdFile myFile;
+
+#define TIMEOUT_UPDATE      1000    // ms
+#define TIMEOUT_DETECT_BATT 2000    // ms
+#define TIMEOUT_SAVE        60000   // ms
+typedef struct{
+    uint32_t update;
+    uint32_t detect_batt;
+    uint32_t button;
+    uint32_t save;
+}SYSTEM_TimeoutTypeDef;
+
+typedef enum{
+    SYSTEM_IDLE = 0,
+    SYSTEM_CHARGE,
+    SYSTEM_LOAD,
+    SYSTEM_FINISH,
+    SYSTEM_OVERTEMP,
+}SYSTEM_Flag_t;
+
+typedef struct{
+    float voltage;
+    float current;
+    float capacity;
+    float temperature;
+}SYSTEM_DataTypeDef;
+
+SYSTEM_TimeoutTypeDef timeout;
+SYSTEM_Flag_t flag;
+SYSTEM_DataTypeDef data;
+
+char display_buffer[20];
+String log_number_str;
 
 void setup(){
     Serial.begin(115200);
@@ -105,85 +136,6 @@ void setup(){
     /* Temperature DS18B20 Setup */
     temperature.begin();
 
-    /* SD Card Setup */
-    if (!SD.begin(53)) { //tergantung di pin chipselect yang digunakan
-        Serial.println("Gagal baca microSD!");
-        return;
-    }
-    Serial.println("Sukses baca kartu microSD!");
-    
-    if (SD.exists("example.txt")) {
-
-        Serial.println("example.txt exists.");
-
-    } else {
-
-        Serial.println("example.txt doesn't exist.");
-
-    }
-
-    // open a new file and immediately close it:
-
-    Serial.println("Creating example.txt...");
-
-    myFile = SD.open("example.txt", FILE_WRITE);
-
-    myFile.close();
-
-    // Check to see if the file exists:
-
-    if (SD.exists("example.txt")) {
-
-        Serial.println("example.txt exists.");
-
-    } else {
-
-        Serial.println("example.txt doesn't exist.");
-
-    }
-
-    // delete the file:
-
-    // Serial.println("Removing example.txt...");
-
-    // SD.remove("example.txt");
-
-    // if (SD.exists("example.txt")) {
-
-    //     Serial.println("example.txt exists.");
-
-    // } else {
-
-    //     Serial.println("example.txt doesn't exist.");
-
-    // }
-
-    // if (!sd.begin(53, SPI_HALF_SPEED)) sd.initErrorHalt();
-
-    // // open the file for write at end like the Native SD library
-    // if (!myFile.open("test.txt", O_RDWR | O_CREAT | O_AT_END)) {
-    //     sd.errorHalt("opening test.txt for write failed");
-    // }
-    // // if the file opened okay, write to it:
-    // Serial.print("Writing to test.txt...");
-    // myFile.println("testing 1, 2, 3.");
-
-    // // close the file:
-    // myFile.close();
-    // Serial.println("done.");
-
-    // // re-open the file for reading:
-    // if (!myFile.open("test.txt", O_READ)) {
-    //     sd.errorHalt("opening test.txt for read failed");
-    // }
-    // Serial.println("test.txt:");
-
-    // // read from the file until there's nothing else in it:
-    // int data;
-    // while ((data = myFile.read()) >= 0) Serial.write(data);
-    // // close the file:
-    // myFile.close();
-
     /* Relay Setup */
     pinMode(RELAY_1_Pin, OUTPUT);
     pinMode(RELAY_2_Pin, OUTPUT);
@@ -199,44 +151,293 @@ void setup(){
 }
 
 void loop(){
-    digitalWrite(RELAY_1_Pin, RELAY_ON);
-    digitalWrite(RELAY_2_Pin, RELAY_OFF);
-    digitalWrite(RELAY_3_Pin, RELAY_OFF);
-    digitalWrite(RELAY_4_Pin, RELAY_OFF);
-    float busVoltage = 0;
-    busVoltage = energy.getBusVoltage_V();
-    Serial.print(busVoltage);
-    Serial.println(" V");
-    
-    delay(1000);
+    updateSensor();
 
-    digitalWrite(RELAY_1_Pin, RELAY_OFF);
-    digitalWrite(RELAY_2_Pin, RELAY_ON);
-    digitalWrite(RELAY_3_Pin, RELAY_OFF);
-    digitalWrite(RELAY_4_Pin, RELAY_OFF);
-    float current = 0; // Measure in milli amps
-    current = energy.getCurrent_mA();
-    Serial.print(current);
-    Serial.println(" mA");
-    delay(1000);
+    if(data.temperature > 50){
+        flag = SYSTEM_OVERTEMP;
+    }
+    else if(data.temperature < 35){
+        flag = SYSTEM_IDLE;
+    }
 
-    digitalWrite(RELAY_1_Pin, RELAY_OFF);
-    digitalWrite(RELAY_2_Pin, RELAY_OFF);
-    digitalWrite(RELAY_3_Pin, RELAY_ON);
-    digitalWrite(RELAY_4_Pin, RELAY_OFF);
-    float power = 0;
-    power = busVoltage * (current/1000); // Calculate the Power
-    Serial.print(power);
-    Serial.println(" W");
-    delay(1000);
+    if((millis() - timeout.update) > TIMEOUT_UPDATE){
+        timeout.update = millis();
 
-    digitalWrite(RELAY_1_Pin, RELAY_OFF);
-    digitalWrite(RELAY_2_Pin, RELAY_OFF);
-    digitalWrite(RELAY_3_Pin, RELAY_OFF);
-    digitalWrite(RELAY_4_Pin, RELAY_ON);
+        if(flag == SYSTEM_CHARGE){
+            systemCharge();
+
+            if(data.voltage > 4.2 && data.current < 100){
+                dataClear();
+                flag = SYSTEM_LOAD;
+            }
+        }
+        else if(flag == SYSTEM_LOAD){
+            systemLoad();
+
+            if(data.voltage < 2.8){
+                flag = SYSTEM_FINISH;
+            }
+        }
+        else if(flag == SYSTEM_FINISH){
+            systemFinish();
+        }
+        else if(flag == SYSTEM_OVERTEMP){
+            systemOverTemp();
+        }
+        else{
+            systemIdle();
+
+            if(data.voltage > 1.0){
+                if((millis() - timeout.detect_batt) > TIMEOUT_DETECT_BATT){
+                    createNewLogNumber();
+                    createNewLog();
+                    flag = SYSTEM_CHARGE;
+                }
+            }
+            else{
+                timeout.detect_batt = millis();
+            }
+        }
+    }
+
+    if(flag == SYSTEM_CHARGE || flag == SYSTEM_LOAD){
+        if((millis() - timeout.save) > TIMEOUT_SAVE){
+            timeout.save = millis();
+
+            saveLog();
+        }
+    }
+    else{
+        timeout.save = millis();
+    }
+}
+
+void updateSensor(void){
+    data.voltage = energy.getBusVoltage_V();
+    Serial.print(data.voltage);     Serial.println(" V");
+
+    data.current = energy.getCurrent_mA();       // Measure in milli amps
+    Serial.print(data.current);     Serial.println(" mA");
+
+    data.capacity += data.current / 3600;
+    Serial.print(data.capacity);    Serial.println(" mAh");
+
     temperature.requestTemperatures();
-    float suhu = temperature.getTempCByIndex(0);
-    Serial.print(suhu);
-    Serial.println(" C");
-    delay(1000);
+    data.temperature = temperature.getTempCByIndex(0);
+    Serial.print(data.temperature); Serial.println(" C");
+}
+
+void systemIdle(void){
+    digitalWrite(RELAY_BATT, RELAY_OFF);
+    digitalWrite(RELAY_CHARGE_BATT, RELAY_OFF);
+    digitalWrite(RELAY_CHARGE_SUPPLY, RELAY_OFF);
+    digitalWrite(RELAY_LOAD, RELAY_OFF);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Insert Battery");
+    lcd.setCursor(0,1);
+    lcd.print("Press Button");
+}
+
+void systemCharge(void){
+    digitalWrite(RELAY_LOAD, RELAY_OFF);
+
+    digitalWrite(RELAY_BATT, RELAY_ON);
+    digitalWrite(RELAY_CHARGE_BATT, RELAY_ON);
+    digitalWrite(RELAY_CHARGE_SUPPLY, RELAY_ON);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    sprintf(display_buffer, "Charge %0f C", data.temperature);
+    lcd.print(display_buffer);
+    lcd.setCursor(0,1);
+    sprintf(display_buffer, "%.02f V  %04f mA", data.voltage, data.current);
+    lcd.print(display_buffer);
+}
+
+void systemLoad(void){
+    digitalWrite(RELAY_CHARGE_BATT, RELAY_OFF);
+    digitalWrite(RELAY_CHARGE_SUPPLY, RELAY_OFF);
+    
+    digitalWrite(RELAY_BATT, RELAY_ON);
+    digitalWrite(RELAY_LOAD, RELAY_ON);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    sprintf(display_buffer, "Load %0f mAh", data.capacity);
+    lcd.print(display_buffer);
+    lcd.setCursor(0,1);
+    sprintf(display_buffer, "%.02f V  %04f mA", data.voltage, data.current);
+    lcd.print(display_buffer);
+}
+
+void systemFinish(){
+    digitalWrite(RELAY_BATT, RELAY_OFF);
+    digitalWrite(RELAY_CHARGE_BATT, RELAY_OFF);
+    digitalWrite(RELAY_CHARGE_SUPPLY, RELAY_OFF);
+    digitalWrite(RELAY_LOAD, RELAY_OFF);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    uint16_t log = getLogNumber();
+    sprintf(display_buffer, "Finish %d", log);
+    lcd.print(display_buffer);
+    lcd.setCursor(0,1);
+    sprintf(display_buffer, "Capacity %0f mAh", data.capacity);
+    lcd.print(display_buffer);
+}
+
+void systemOverTemp(){
+    digitalWrite(RELAY_BATT, RELAY_OFF);
+    digitalWrite(RELAY_CHARGE_BATT, RELAY_OFF);
+    digitalWrite(RELAY_CHARGE_SUPPLY, RELAY_OFF);
+    digitalWrite(RELAY_LOAD, RELAY_OFF);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Over Temperature");
+    lcd.setCursor(0,1);
+    sprintf(display_buffer, "Temp %0f C", data.temperature);
+    lcd.print(display_buffer);
+}
+
+void dataClear(void){
+    data.capacity = 0;
+}
+
+uint16_t getLogNumber(void){
+    uint16_t ret = 0;
+
+    if (SD.begin(SD_CS_Pin)) {
+        File dataFile = SD.open(LOG_NUMBER_FILE);
+    
+        if (dataFile) {
+            log_number_str = "";
+
+            while(dataFile.available()){
+                char a = dataFile.read();
+                log_number_str += a;
+            }
+            ret = log_number_str.toInt();
+            ret++;
+
+            dataFile.close();
+        }
+        else {
+            Serial.print("Fail to Open");
+            Serial.println();
+        }   
+    }
+    else{
+        Serial.println("SD isn't Present.");
+    }
+
+    SD.end();
+
+    return ret;
+}
+
+uint16_t createNewLogNumber(void){
+    uint16_t ret = 0;
+
+    if (SD.begin(SD_CS_Pin)) {
+        File dataFile = SD.open(LOG_NUMBER_FILE);
+    
+        if (dataFile) {
+            log_number_str = "";
+
+            while(dataFile.available()){
+                char a = dataFile.read();
+                log_number_str += a;
+            }
+            ret = log_number_str.toInt();
+            ret++;
+
+            dataFile.close();
+
+            for(uint8_t i=0; i<3; i++){
+                SD.remove(LOG_NUMBER_FILE);
+                if(!SD.exists(LOG_NUMBER_FILE)){
+                    break;
+                }
+            }
+        }
+        else {
+            Serial.print("Fail to Open");
+            Serial.println();
+        }   
+
+        dataFile = SD.open(LOG_NUMBER_FILE, FILE_WRITE);
+
+        if(dataFile){
+            dataFile.print(ret);
+            dataFile.close();
+        }
+    }
+    else{
+        Serial.println("SD isn't Present.");
+    }
+
+    SD.end();
+
+    return ret;
+}
+
+void createNewLog(void){
+    uint16_t log = getLogNumber();
+    sprintf(display_buffer, "%d.csv", log);
+    
+    if (SD.begin(SD_CS_Pin)) {
+        if(SD.exists(display_buffer)){
+            for(uint8_t i=0; i<3; i++){
+                SD.remove(display_buffer);
+                if(!SD.exists(display_buffer)){
+                    break;
+                }
+            }
+        }
+
+        File dataFile = SD.open(display_buffer, FILE_WRITE);
+    
+        if (dataFile) {
+            dataFile.println("volage (V),current (mA),capacity (mAh),temperature (C)");
+            dataFile.close();
+        }
+        else {
+            Serial.print("Fail to Open");
+            Serial.println();
+        }   
+    }
+    else{
+        Serial.println("SD isn't Present.");
+    }
+
+    SD.end();
+}
+
+void saveLog(void){
+    uint16_t log = getLogNumber();
+    sprintf(display_buffer, "%d.csv", log);
+    
+    if (SD.begin(SD_CS_Pin)) {
+        File dataFile = SD.open(display_buffer, FILE_WRITE);
+    
+        if (dataFile) {
+            sprintf(display_buffer, "%.02f,%04f", data.voltage, data.current);
+            dataFile.print(display_buffer);
+            sprintf(display_buffer, "%04f,%02f", data.capacity, data.temperature);
+            dataFile.println(display_buffer);
+            dataFile.close();
+        }
+        else {
+            Serial.print("Fail to Open");
+            Serial.println();
+        }   
+    }
+    else{
+        Serial.println("SD isn't Present.");
+    }
+
+    SD.end();
 }
